bca24b956ff4aa0eab029dfd4a189bcca71ded03
hwhhemm <159470517+epltv1@users.noreply.github.com>
2025-06-07 23:46:02 +0300

    Update auth.js

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,13 +1,15 @@
 function checkAuthStatus() {
-  const token = localStorage.getItem('authToken');
+  const vipSession = localStorage.getItem('vipSession');
+  const adminToken = localStorage.getItem('adminToken');
   const loginLink = document.getElementById('loginLink');
   const logoutButton = document.getElementById('logoutButton');
-  if (token) {
+  if (vipSession || adminToken) {
     loginLink.classList.add('d-none');
     logoutButton.classList.remove('d-none');
     logoutButton.addEventListener('click', () => {
-      localStorage.removeItem('authToken');
-      window.location.reload();
+      localStorage.removeItem('vipSession');
+      localStorage.removeItem('adminToken');
+      window.location.href = '/login';
     });
   } else {
     loginLink.classList.remove('d-none');
